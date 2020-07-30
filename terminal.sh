20969b88d94a1532423605c2d7918b0d80c1d6d1
Maurício Magalhães <contato@mauriciomagalhaes.com.br>
2020-07-30 13:47:12 -0300

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -12,6 +12,10 @@ echo ""
 
 if [ "$SESSIONEXISTS" = "" ]
 then
+        echo ""
+        echo "Não existe sessões, aguarde ..."
+        echo ""
+
         tmux new-session -d -s $SESSION
 
         tmux rename-window 'Terminal SIP'
@@ -21,33 +25,38 @@ then
         tmux select-pane -t 0
         tmux split-window -h
         tmux resize-pane -L 30
-        tmux resize-pane -U 4
+        tmux resize-pane -U 10
 
         tmux select-pane -t 2
         tmux split-window -v
 
         tmux select-pane -t 3
-        #tmux split-window -h
-        #tmux resize-pane -L 40
+        tmux resize-pane -U 5
+        tmux split-window -v
 
         #
         tmux select-pane -t 0
         tmux send-key "mtr $TRACEROUTEIP" C-m
         #
         tmux select-pane -t 1
-        tmux send-key "watch -n 1 -t 'rasterisk -x "'"sip show channels"'" | grep ACK'" C-m
+        tmux send-key "watch -n 1 'rasterisk -x "'"sip show channels"'" | grep ACK'" C-m
         #
         tmux select-pane -t 2
-        tmux send-key "watch -n 1 -t 'rasterisk -x "'"sip show channelstats"'"'" C-m
+        tmux send-key "watch -n 1 'rasterisk -x "'"sip show channelstats"'"'" C-m
         #
-        tmux select-pane -t 3
-        #tmux send-key "watch -n 1 -t 'rasterisk -x "'"core show channels concise"'" | grep -v sms'" C-m
+        tmux select-pane -t 4
         tmux send-key "watch -n 1 -t 'rasterisk -x "'"core show channels concise"'"'" C-m
+        #tmux send-key "watch -n 1 'rasterisk -x "'"core show channels concise"'" | grep -v sms'" C-m
+
+        tmux select-pane -t 3
+        tmux send-key "watch -n 1 -c 'rasterisk -x "'"queue show"'"'" C-m
 
         tmux new-window -t $SESSION:1 -n 'Shell'
         tmux select-window -p
         tmux select-pane -t 0
         tmux attach
 fi
+
 tmux attach-session -t $SESSION
+tmux select-pane -t 0
 
